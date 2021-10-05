f18f1893f5310abfec3dc71531e5dfc3be42fbfd
abfkh <zpiiouy@gmail.com>
2021-10-05 13:44:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// imports
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+
+//static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+    // set views
+
+// app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    res.render('layout.ejs')
+})
+
+
+app.get('/products', (req, res) => {
+    res.render('products')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listen on port
+app.listen(port, () => console.info(`Listening on port ${port}`))
